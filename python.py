ba7bf32e0ef9917eacb8493bedf1babf0cb704b5
Tanmay Mittal <tanmay98972@gmail.com>
2026-09-01 10:14:21 +0530

    hello

---
+++ python.py
@@ -0,0 +1,34 @@
+# c=bool("")+34-4
+# print(c)
+# str="python"
+# print(str[0])
+
+# print(str[len(str)-1])
+# print(str[-1])
+
+# print(str[-len(str)])
+
+# for i in range(len(str)):
+#     print(str[i])
+# for j in range(-len(str),0):
+#     print(str[j])
+
+# for s in str:
+#     print(s)
+
+# Str="python in GLA CL2"
+# # print(Str)
+# # print(Str[::])
+# print(Str[-5:9:-1])
+# print(Str[-100:100])
+
+# from sys import argv
+# # print(argv[0])
+# # print(argv[1])
+# # print(argv[2])
+# # print(argv[3])
+# print(len(argv))
+
+print("Python"+" in GLA CL2")
+print("python"*5)
+print(3.5*"python")
