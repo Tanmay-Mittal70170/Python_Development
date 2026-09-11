fc5cc936f063614ac6ebcf315db92046c8cd152e
Tanmay Mittal <tanmay98972@gmail.com>
2026-09-11 11:37:42 +0530

    Hello

---
+++ Questions/Question-07.py
@@ -0,0 +1,7 @@
+#Question7: To take the string from the user and then take even position charactor on way and odd position other way:
+
+s=input("enter the string")
+c=s[::2]
+print("even position characters are",c)
+h=s[1::2]
+print("odd positions characters are",h)
