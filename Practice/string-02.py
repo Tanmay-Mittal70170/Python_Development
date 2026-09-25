189db5b8662f6bcc4f0999bc17bc2c83c6ff5ce7
Tanmay Mittal <tanmay98972@gmail.com>
2026-09-25 10:32:54 +0530

    hello

--- Practice/string-02.py
+++ Practice/string-02.py
@@ -70,15 +70,15 @@
 #     if is_prime==True:
 #       print(num)
 
-L=[]
+# L=[]
 
-for x in range(3):
-    rollno=int(input("enter the roll no"))
-    name=input("enter the name")
-    email=input("enter the email")
-    phone=int(input("enter the phone number"))
-    t=rollno, name, email, phone
-    L.append(t)
+# for x in range(3):
+#     rollno=int(input("enter the roll no"))
+#     name=input("enter the name")
+#     email=input("enter the email")
+#     phone=int(input("enter the phone number"))
+#     t=rollno, name, email, phone
+#     L.append(t)
 
-for r,n,e,p in L:
-    print("%5d %-15s %20s %10d"%(r,n,e,p))
+# for r,n,e,p in L:
+#     print("%5d %-15s %20s %10d"%(r,n,e,p))
